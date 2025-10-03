d79a6145641bb29b670fb7506b5bd132fd92180a
YUTIAN BAI <yutian_bai@outlook.com>
2025-10-03 11:10:42 -0700

    Migrating my old files over

---
+++ Week 3/reading.js
@@ -0,0 +1,63 @@
+// function greet(name){
+// 	console.log("Ran greet", name);
+// 	return "Hello, " + name;
+// }
+
+// console.log(greet("Yokis"));
+
+
+// function numberAdder(firstNumber, secondNumber) {
+	// let sum;
+	// sum = firstNumber + secondNumber;
+	// console.log(sum);
+	// return sum;
+// }
+
+// console.log(numberAdder(10,5));
+
+
+// let globleVariable = "I am a global variable";
+
+// function showScope() {
+	// let localVariable = "I am a global variable";
+	// console.log(localVariable);
+// }
+
+// console.log(globleVariable);
+
+// showScope();
+
+
+
+
+let cityArray = ['Rome', 'Los Angeles', 'Pasadena', 'Kyoto'];
+console.log(cityArray);
+
+for (var i = 0; i < cityArray.length; i++){
+	console.log(cityArray[i]);
+ }
+
+ 	if (cityArray[i] === "Pasadena") {
+ 		console.log("I LOVE IT!!!");
+ 	} else if (cityArray[i] === "Los Angeles") {
+ 		console.log("YEEEEE");
+ 	} else {
+ 		console.log("abcdefg");
+ 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
